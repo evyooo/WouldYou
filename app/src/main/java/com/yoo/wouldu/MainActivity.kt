23012ed06378d605b5b1b34b8e26c889314fe44d
evyooo <yooooojining@gmail.com>
2021-11-22 18:12:28 +0900

    feat(main): bottom navigation bar 설정

--- app/src/main/java/com/yoo/wouldu/MainActivity.kt
+++ app/src/main/java/com/yoo/wouldu/MainActivity.kt
@@ -2,7 +2,12 @@ package com.yoo.wouldu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.yoo.wouldu.databinding.ActivityMainBinding
+import com.yoo.wouldu.view.ChatFragment
+import com.yoo.wouldu.view.HomeFragment
+import com.yoo.wouldu.view.MyFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +16,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navigationItemSelect()
     }
+
+private fun replaceFragment(fragment: Fragment) {
+    val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+    fragmentTransaction.replace(R.id.holder_fl_main, fragment)
+    fragmentTransaction.commit()
+}
+
+private fun navigationItemSelect() {
+    binding.bnMain.run {
+        setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.action_home -> replaceFragment(HomeFragment())
+                R.id.action_chat -> replaceFragment(ChatFragment())
+                R.id.action_my -> replaceFragment(MyFragment())
+            }
+            true
+        }
+        selectedItemId = R.id.action_home
+    }
+}
 }
---
+++ app/src/main/java/com/yoo/wouldu/view/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.yoo.wouldu.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.yoo.wouldu.R
+import com.yoo.wouldu.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+    private val TAG = "HomeFragment"
+
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+}
