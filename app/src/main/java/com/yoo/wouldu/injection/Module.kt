7993cd8e07334e21cc564e6d0ac8fe4c41ad3913
evyooo <yooooojining@gmail.com>
2021-11-24 01:18:15 +0900

    chore(koin): 초기 세팅

---
+++ app/src/main/java/com/yoo/wouldu/injection/Module.kt
@@ -0,0 +1,14 @@
+package com.yoo.wouldu.injection
+
+import com.yoo.wouldu.viewmodel.RequestViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import org.koin.android.ext.koin.androidApplication
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    // Request
+    viewModel { RequestViewModel(get()) }
+}
--- app/src/main/java/com/yoo/wouldu/viewmodel/RequestViewModel.kt
+++ app/src/main/java/com/yoo/wouldu/viewmodel/RequestViewModel.kt
@@ -1,8 +1,9 @@
 package com.yoo.wouldu.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.yoo.wouldu.model.RequestRepository
 
-class RequestViewModel : ViewModel() {
+class RequestViewModel(private val repository: RequestRepository) : ViewModel() {
     private val TAG = "RequestViewModel"
 
 
